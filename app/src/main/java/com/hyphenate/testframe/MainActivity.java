3013d16bd13b65de5d93c1294c542ca89f1033d7
hub9527 <yes5921@126.com>
2017-03-19 15:12:12 -0800

    modify

--- app/src/main/java/com/hyphenate/testframe/MainActivity.java
+++ app/src/main/java/com/hyphenate/testframe/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("main","test");
-        Log.d("main","testddd");
+        Log.d("main","tesddd");
 
     }
 }
