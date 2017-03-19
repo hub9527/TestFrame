d87cbe12596e919e412fcb5674c6fc8932dc99ce
hub9527 <yes5921@126.com>
2017-03-19 15:14:27 -0800

    New changelist

--- app/src/main/java/com/hyphenate/testframe/MainActivity.java
+++ app/src/main/java/com/hyphenate/testframe/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("main","test");
-        Log.d("main","tesdd");
+        Log.d("main","tesyudd");
 
     }
 }
