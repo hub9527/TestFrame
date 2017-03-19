1cc95467bb2aa0e4f7ea7b6470d936aa92df8a3a
hub9527 <yes5921@126.com>
2017-03-19 15:53:45 -0800

    add a class

--- app/src/main/java/com/hyphenate/testframe/MainActivity.java
+++ app/src/main/java/com/hyphenate/testframe/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("main","tet000");
         Log.d("main","tesyudd");
+        Log.d("main","tesyudd");
+        Log.d("main","tesyudd");
 
     }
 }
